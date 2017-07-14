421ea937fd9fd3918004e83f971810e9858c0406
tsutsu <tsutsu0928@gmail.com>
2017-07-15 04:32:19 +0900

    ネットワークを変更．

--- model.py
+++ model.py
@@ -7,14 +7,25 @@ class CNN(chainer.Chain):
     CLASSES = 4
 
     def __init__(self, train=True):
-        super(CNN, self).__init__(
-            conv1=L.Convolution2D(3, 32, 5),
-            conv2=L.Convolution2D(32, 64, 5),
-            l1=L.Linear(10816, CNN.CLASSES)
-        )
-        self.train = train
+        super(CNN, self).__init__()
+        with self.init_scope():
+            self.conv1 = L.Convolution2D(None, 96, 11, stride=4)
+            self.conv2 = L.Convolution2D(None, 256,  5, pad=2)
+            self.conv3 = L.Convolution2D(None, 384,  3, pad=1)
+            self.conv4 = L.Convolution2D(None, 384,  3, pad=1)
+            self.conv5 = L.Convolution2D(None, 256,  3, pad=1)
+            self.fc6 = L.Linear(None, 4096)
+            self.fc7 = L.Linear(None, 4096)
+            self.fc8 = L.Linear(None, CNN.CLASSES)
 
     def __call__(self, x):
-        h = F.max_pooling_2d(F.relu(self.conv1(x)), 2)
-        h = F.max_pooling_2d(F.relu(self.conv2(h)), 2)
-        return self.l1(h)
+        h = F.max_pooling_2d(F.local_response_normalization(
+            F.relu(self.conv1(x))), 3, stride=2)
+        h = F.max_pooling_2d(F.local_response_normalization(
+            F.relu(self.conv2(h))), 3, stride=2)
+        h = F.relu(self.conv3(h))
+        h = F.relu(self.conv4(h))
+        h = F.max_pooling_2d(F.relu(self.conv5(h)), 3, stride=2)
+        h = F.dropout(F.relu(self.fc6(h)))
+        h = F.dropout(F.relu(self.fc7(h)))
+        return self.fc8(h)
