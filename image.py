5e34e031a505b40286fa89b007f7ae1e85d27a43
tsutsu <tsutsu0928@gmail.com>
2017-07-18 21:45:50 +0900

    画像の切り出し処理を改善．

--- image.py
+++ image.py
@@ -18,76 +18,38 @@ def show_grayimg(img):
     plt.show()
 
 
-def draw_hole_black(img):
-    '''
-    5円，50円の穴を黒でぬる．
-    '''
-    img = np.copy(img)
-    w_min = np.array([150, 150, 150], np.uint8)
-    w_max = np.array([255, 255, 255], np.uint8)
-    bin_img = cv2.inRange(img, w_min, w_max)
-    # show_grayimg(hoge_img)
-
-    _, coins_contours, __ = cv2.findContours(
-        bin_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    min_coin_area = 60
-
-    def inner_check(img, contour):
-        '''
-        contourがimgの中央あたりにあるかチェックする．（5円，50円のくり抜き用）
-        '''
-        def inner(a, b):
-            return a * 0.8 < b and b < a * 1.2
-        height, width, depth = img.shape
-        x, y, w, h = cv2.boundingRect(contour)
-        return inner(x, y) and inner(x, width - (x + w)) and inner(y, height - (y + h)) and w < width * 0.35
-
-    large_contours = [x for x in coins_contours if min_coin_area <
-                      cv2.contourArea(x) and inner_check(img, x)]
-
-    for x in large_contours:
-        cv2.fillConvexPoly(img, x, (0, 0, 0))  # 黒背景のベースにコイン部を白で描画
-
-    return img
-
-
 def clip_coin(filename):
     '''
     バウンディングボックスを描画した画像と，その部分を切り取った画像リストをtupleで戻す．
     '''
     img = cv2.imread(filename)
-    h, w, d = img.shape
-
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gaus_img = cv2.GaussianBlur(gray_img, (5, 5), 0)
-    is_white_base = 128 < gaus_img[10, 10]
-    _, bin_img = cv2.threshold(
-        gaus_img, 192 if is_white_base else 60, 255, cv2.THRESH_BINARY)
+    ret, bin_img = cv2.threshold(
+        gray_img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-    if is_white_base:
-        bin_img = cv2.bitwise_not(bin_img)    # 背景が白の場合は必要
+    a = bin_img.mean()
+
+    if 255 / 2 < bin_img.mean():
+        bin_img = cv2.bitwise_not(bin_img)
 
     _, coins_contours, __ = cv2.findContours(
         bin_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    mask_img = np.zeros(img.shape, dtype=np.uint8)  # 黒背景のベース
-    min_coin_area = 60
+    min_coin_area = 10000
     large_contours = [
         x for x in coins_contours if min_coin_area < cv2.contourArea(x)]
-    for x in large_contours:
-        cv2.fillConvexPoly(mask_img, x, (255, 255, 255))    # 黒背景のベースにコイン部を白で描画
 
-    masked_img = cv2.bitwise_and(img, mask_img)     # mask_imgの白部分のみimgを描画
+    def inner_check(a, b):
+        return a * 0.8 < b and b < a * 1.2
 
     clip_imgs = []
     boundingbox_img = np.copy(img)
     for contour in large_contours:
         x, y, w, h = cv2.boundingRect(contour)
+        if not inner_check(w, h):
+            continue
         n = min(w, h)
-
-        clip_img = np.copy(masked_img[y:y + n, x:x + n])
-        clip_img = draw_hole_black(clip_img)
-        clip_imgs.append(clip_img)
-        cv2.rectangle(boundingbox_img, (x, y), (x + n, y + n), (0, 255, 0), 2)
+        clip_imgs.append(img[y:y + n, x:x + n])
+        cv2.rectangle(boundingbox_img, (x, y), (x + n, y + n), (0, 255, 0), 10)
 
     return boundingbox_img, clip_imgs
 
@@ -99,6 +61,7 @@ def clip_all(root):
     if os.path.isdir(outdir):
         shutil.rmtree(outdir)
     os.mkdir(outdir)
+    count = 0
     for f in os.listdir(root):
         filename = os.path.join(root, f)
         if os.path.isfile(filename):
@@ -108,8 +71,8 @@ def clip_all(root):
                 path, fname = os.path.split(filename)
                 body, exe = os.path.splitext(fname)
                 savename = os.path.join(
-                    outdir, '{}_{}{}'.format(body, id(img), '.png'))
-
+                    outdir, '{}_{}{}'.format(body, count, '.png'))
+                count += 1
                 cv2.imwrite(savename, resize_img)
 
 
